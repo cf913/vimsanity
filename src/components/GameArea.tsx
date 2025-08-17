7b79b265765f20e9ce7138cfcbadc6413d90ca8a
cf913 <chaddudz@hotmail.fr>
2025-08-17 19:58:00 +1000

    0.0.17

--- src/components/GameArea.tsx
+++ src/components/GameArea.tsx
@@ -18,12 +18,11 @@ interface GameAreaProps {
 }
 
 // Changelog version and message
-const GAME_VERSION = '0.0.16'
+const GAME_VERSION = '0.0.17'
 
 const CHANGELOG_MESSAGE = `
 <b>What's New in ${GAME_VERSION}</b><br/><br/>
-- Added level 9: Undo and Redo<br/><br/>
-- Back from holidays, more updates coming soon!
+- Added level 10: basic single key delete commands<br/><br/>
 Thanks for playing!
 `
 
