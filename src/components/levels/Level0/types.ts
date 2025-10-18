59063e39045d2717b0f04c11ff85e860725bcdbd
cf913 <chaddudz@hotmail.fr>
2025-10-18 19:52:32 +1000

    happy

--- src/components/levels/Level0/types.ts
+++ src/components/levels/Level0/types.ts
@@ -24,7 +24,7 @@ export interface VimCommand {
   colorClass: string
 }
 
-export type ProficiencyLevel = 'beginner' | 'intermediate' | 'advanced' | 'expert' | 'insanity'
+export type ProficiencyLevel = 'beginner' | 'intermediate' | 'advanced' | 'vim-btw'
 
 export interface ProficiencyPreset {
   level: ProficiencyLevel
